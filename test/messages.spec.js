374f8a806cc43840634ccd1ce7ab4f1177784f86
Gingertonic <beth@getfutureproof.co.uk>
2021-09-17 17:09:42 +0100

    print word in rejection tests

--- test/messages.spec.js
+++ test/messages.spec.js
@@ -1,16 +1,18 @@
+const messageServices = require("../messages");
+
 describe("public annoucement service", () => {
     it("prepares a public announcement", () => {
         let msg = "Don't lie in yellow snow";
-        let annoucement = (psa(msg));
+        let annoucement = messageServices.psa(msg);
         expect(annoucement).toContain(msg);
     })
 
     describe.each([
         "poop", "fart", "bum", "jquery"
-    ])("throws error rejecting messages containing rude words", () => {
-        it('rejects %s', () => {
-            let msg = "This is about %s haha!"
-            expect(psa(msg)).toThrowError();
+    ])("throws error rejecting messages containing rude words", (word) => {
+        it(`rejects ${word}`, () => {
+            let msg = `This is about ${word} haha!`
+            expect(messageServices.psa(msg)).toThrowError();
         })
     })
 })
