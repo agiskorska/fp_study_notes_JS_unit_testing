e83040aa993f2bf3210d4af4d98ea2eab3d6d619
Gingertonic <beth@getfutureproof.co.uk>
2021-09-17 17:31:46 +0100

    normalise message for filter

--- messages.js
+++ messages.js
@@ -2,7 +2,7 @@ function psa(msg){
     let bannedWords = ["poop", "fart", "bum", "jquery"];
 
     for(let w of bannedWords){
-        if(msg.includes(w)) { throw Error("That's too rude for public consumption!")}
+        if(msg.toLowerCase().includes(w)) { throw Error("That's too rude for public consumption!")}
     }
     return `This is a Public Service Annoucment: ${msg}!`;
 }
